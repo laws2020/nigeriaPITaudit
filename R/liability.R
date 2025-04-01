998e67d1d927f56a36b7010b644e5e1adc8993fb
Lawrence Daniel <laws.garba@gmail.com>
2025-04-01 03:58:08 +0100

    Initial commit

--- R/liability.R
+++ R/liability.R
@@ -7,17 +7,17 @@
 #' Taxable income is the amount of income that will be subject to tax after all allowable
 #' deductions and reliefs have been applied. It is a crucial figure for determining the tax liability.
 #'
-#' @param gross_emolument Numeric. The total earnings before deductions.
+#' @param gross_salary Numeric. The total earnings before deductions computed by \code{gross_salary()}.
 #' @param total_relief Numeric. The aggregate relief computed by \code{total_relief()}.
 #'
-#' @return A numeric value representing the taxable income. If deductions exceed gross emolument, the result is 0.
+#' @return A numeric value representing the taxable income. If deductions exceed gross salary, the result is 0.
 #'
 #' @examples
 #' taxable_income(5000000, 1000000)  # Returns 4000000
 #'
 #' @export
-taxable_income <- function(gross_emolument, total_relief){
-  return(gross_emolument - total_relief)
+taxable_income <- function(gross_salary, total_relief){
+  return(gross_salary - total_relief)
 }
 
 
